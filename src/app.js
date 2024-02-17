6f8d2c609cca3b85121d7af69f6e39a1f9b5723a
Redho Arifin <redhoarifin2017@gmail.com>
2024-02-17 21:47:33 +0900

    fix axios

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-const {axios} = require('axios');
-const Message = 'Asu babi elu';
-const img = "https://www.klinikindonesia.com/image/t/tinja.jpg";
-// const endpoint =
-//   "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendMessage?parse_mode=markdown&chat_id=6618165018&text=" +
-//   Message;
-const endpoint2 = 
-"https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendPhoto?chat_id=6618165018&photo="+img+"&caption="+Message;
+const {default: axios} = require('axios');
 
-// axios.get(endpoint).then((res) => {
-//     console.log(res.data);
-// })
+
+const Message = 'Babi lu, kontol, ASU!!!!!';
+const img = "https://www.klinikindonesia.com/image/t/tinja.jpg";
+const endpoint =
+  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendMessage?parse_mode=markdown&chat_id=6618165018&text=" +
+  Message;
+const endpoint2 =
+  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendPhoto?chat_id=6618165018&photo=" +
+  img +
+  "&caption=" +
+  Message;
 
 async function sendMessage(){
     try {
         while (true) {
             await axios.get(endpoint2)
             .then((res) => {
-                console.log(res.data);
+              
                 if (res.status===200) {
                     console.log(res.data);
+                }  else {
+                    console.log("message failed!")
                 }
             })
         }
     }catch (error) {
-        console.log("API-key Down");
+        console.log("API-key Down",error);
     }
 }
 
