3c6a97d4c6eb8148e96011357b5f2201b9e192d5
Redho Arifin <redhoarifin2017@gmail.com>
2024-02-19 21:53:09 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 const {default: axios} = require('axios');
 
+//api key dari aplikasi penipu
+const endpoint1 =
+  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendMessage?parse_mode=markdown&chat_id=6618165018&text=";
+const endpoint2 = "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendPhoto?chat_id=6618165018&photo=";
 
-const Message = 'Babi lu, kontol, ASU!!!!!';
+const Message = 'asu!!';
 const img = "https://www.klinikindonesia.com/image/t/tinja.jpg";
-const endpoint =
-  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendMessage?parse_mode=markdown&chat_id=6618165018&text=" +
-  Message;
-const endpoint2 =
-  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendPhoto?chat_id=6618165018&photo=" +
+
+const query =
+  endpoint2 +
   img +
   "&caption=" +
   Message;
 
 async function sendMessage(){
     try {
-        while (true) {
-            await axios.get(endpoint2)
-            .then((res) => {
-                res.status===200 ;
-                    console.log(res.data);
-            })
-        }
+            await axios.get(query).then((res) => {
+              if (res.status === 200) {
+                console.log("Success", res.data);
+              } else {
+                console.log("Failed with status:", res.status);
+              }
+            });
+        
     }catch (error) {
-        console.log("API-key Down");
+        console.log(error);
     }
 }
 
