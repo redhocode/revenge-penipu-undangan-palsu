57fae22ade2db188e5ef1a06d4757ecb9e2a6072
Redho Arifin <redhoarifin2017@gmail.com>
2024-02-21 17:17:18 +0900

    fix

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
 const {default: axios} = require('axios');
 
-//api key dari aplikasi penipu
+//masukan api key dari aplikasi penipu
 const endpoint1 =
-  "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendMessage?parse_mode=markdown&chat_id=6618165018&text=";
-const endpoint2 = "https://api.telegram.org/bot6457148300:AAHd3_gH8SW4VH9kUt3aavIRFVFt8gMrjPs/sendPhoto?chat_id=6618165018&photo=";
+  "";
+const endpoint2 = "";
 
 const Message = 'asu!!';
 const img = "https://www.klinikindonesia.com/image/t/tinja.jpg";
 
-const query =
-  endpoint2 +
-  img +
-  "&caption=" +
-  Message;
+// const query =
+//   endpoint2 +
+//   img +
+//   "&caption=" +
+//   Message;
 
-async function sendMessage(){
+const sendMassage = endpoint1 + Message;
+
+async function attack(){
     try {
-            await axios.get(query).then((res) => {
-              if (res.status === 200) {
-                console.log("Success", res.data);
-              } else {
-                console.log("Failed with status:", res.status);
-              }
-            });
+          const response =  await axios.get(sendMassage)
+             
+                console.log("Success", response.data);
+           
+            
         
     }catch (error) {
         console.log(error);
+        
     }
 }
 
-setInterval(sendMessage, 5000);
+setInterval(attack, 5000);
